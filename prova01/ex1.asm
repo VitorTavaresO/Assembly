727ea9deb36d3bf3a15393ed2a1b4f0820acea72
VitorTavaresO <vitortavares.o55@gmail.com>
2023-05-26 21:00:02 -0300

    Ex1 AV1

--- prova01/ex1.asm
+++ prova01/ex1.asm
@@ -1,42 +1,43 @@
 .data
     vector: .word 0 0 0 0 0 0 0 0 0
-    n:  .word 9 
-    hyphen: .asciiz "-" 
+    n:  .word 9
+    hyphen: .asciiz "-"
     newline: .asciiz "\n"
 
 .text
 
 main:
-    la      $t1, vector
-    lw 	    $t2, n
-    li      $t3, 1
+    la      $t1, vector ## $t1 = &vector[]
+    lw 	    $t2, n     ## $t2 = n = 9
+    li      $t3, 1    ## $t3 = 1 (Valor inicial do vetor)
+
 loop:
-    bge     $t0, $t2, exit
+    bge     $t0, $t2, exit ## Se $t0 >= $t2, pula para o exit
 
-    mul     $t4, $t3, $t3
-    sw      $t4, ($t1)
-    add     $t1, $t1, 4 
+    mul     $t4, $t3, $t3 ## $t4 = $t3 * $t3
+    sw      $t4, ($t1)   ## vector[$t1] = $t4
+    add     $t1, $t1, 4 ## $t1 = $t1 + 4 (Move para o próximo elemento do vetor)
 
-    li      $v0, 1      
-    move    $a0, $t3
+    li      $v0, 1      ## Imprime valor inteiro
+    move    $a0, $t3    ## $a0 = $t3
     syscall
 
-    li      $v0, 4
-    la      $a0, hyphen
+    li      $v0, 4     ## Imprime string
+    la      $a0, hyphen ## $a0 = hyphen = "-"
     syscall
 
-    li      $v0, 1      
-    move    $a0, $t4
+    li      $v0, 1     ## Imprime valor inteiro
+    move    $a0, $t4  ## $a0 = $t4
     syscall
 
-    li      $v0, 4
-    la      $a0, newline
+    li      $v0, 4   ## Imprime string
+    la      $a0, newline ## $a0 = newline = "\n" (Pular uma linha)
     syscall
 
-    add     $t0, $t0, 1
-    add     $t3, $t3, 1
-    j       loop
+    add     $t0, $t0, 1 ## Incrementa o contador do laço
+    add     $t3, $t3, 1 ## Incrementa o valor do vetor
+    j       loop       ## Volta para o início do laço
 
  exit:
-    li      $v0, 10
+    li      $v0, 10    ## Termina o programa
     syscall
