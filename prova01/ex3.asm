1d7e86e721d5145d0cc585b132bf41ab5f77dbf5
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-21 15:50:53 -0300

    Correction

--- prova01/ex3.asm
+++ prova01/ex3.asm
@@ -1,5 +1,5 @@
 .data
-    vector: .word  18 33 61 52 2
+    vector: .word  18 51 61 2 33
     n:  .word 5
     vector_string: .asciiz "Vetor: "
     vector_ordened_String: .asciiz "Vetor ordenado: "
@@ -15,6 +15,7 @@ main:
     li      $t4, 0              ## $t4 = 1
 	move 	$t6, $t2
 	li 		$t9, 0
+	
 
 inner_loop:
 	bge     $t4, $t1, swap      ## Se $t0 >= $t1, pula para o exit
@@ -32,11 +33,13 @@ define_index:
 	j 		inner_loop
 
 swap:
-	mul 	$t8, $t1, -4 	  ## $t5 = $t8 * 4
-	add 	$t0, $t0, $t8  	  ## $t0 = $t0 - 4
-	sw      $t2, ($t0)          ## vector[i] = $t2
-	add 	$t0, $t0, $t5 	  ## $t0 = $t0 + 4
+	mul 	$t8, $t4, -4 	  ## $t8 = $t1 * 4 = -20
+	add 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
+	sw      $t2, ($t0)       ## vector[i] = $t2
+	add 	$t0, $t0, $t5 	## $t0 = $t0 + $t5
 	sw 		$t6, ($t0)        ## vector[i+1] = $t3
+	add 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
+
 
 exit:
 
