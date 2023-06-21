2578c3ac1783f39f5d14a05f4112cfb81a152ee4
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-21 16:45:03 -0300

    Almost

--- prova01/ex3.asm
+++ prova01/ex3.asm
@@ -10,15 +10,17 @@
 main:
     la      $t0, vector         ## $t0 = &vector[]
     lw 	    $t1, n              ## $t2 = n = 5
-    lw      $t2, ($t0)          ## $t2 = vector[i] - Menor Valor do Vetor
-    lw      $t3, 4($t0)         ## $t3 = vector[i+1]
-    li      $t4, 0              ## $t4 = 1
-	move 	$t6, $t2
-	li 		$t9, 0
-	
+	add 	$t7, $t1, -1 	  ## $t7 = $t1 - 1 = 4
+	li 		$t9, 1
 
+outer_loop:	
+	bgt     $t9, $t1, exit      ## Se $t9 >= $t1, pula para o exit
+	lw      $t2, ($t0)          ## $t2 = vector[i] - Menor Valor do Vetor
+	move 	$t6, $t2
+	li 		$t4, 0
+	
 inner_loop:
-	bge     $t4, $t1, swap      ## Se $t0 >= $t1, pula para o exit
+	bgt     $t4, $t1, swap    ## Se $t4 > $t7, pula para o swap
 	lw      $t3, 4($t0)         ## $t3 = vector[i+1]
 	bgt     $t2, $t3, define_index     ## Se $t2 > $t3, pula para o define_index
 	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
@@ -33,13 +35,21 @@ define_index:
 	j 		inner_loop
 
 swap:
-	mul 	$t8, $t4, -4 	  ## $t8 = $t1 * 4 = -20
-	add 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
+	mul 	$t8, $t4, 4 	  ## $t8 = $t1 * 4 = -20
+	sub 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
 	sw      $t2, ($t0)       ## vector[i] = $t2
 	add 	$t0, $t0, $t5 	## $t0 = $t0 + $t5
 	sw 		$t6, ($t0)        ## vector[i+1] = $t3
-	add 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
+	sub 	$t0, $t0, $t5 	## $t0 = $t0 - $t5
 
+end_outer_loop:
+	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
+	add 	$t9, $t9, 1 	  ## $t9 = $t9 + 1
+
+	li 		$v0, 1
+	move 	$a0, $t2
+	syscall
+	j 		outer_loop
 
 exit:
 
