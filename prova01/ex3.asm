b01ba2c4ef41b03738a1446f166732af03fffca6
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-21 14:07:01 -0300

    Inner Loop Swap

--- prova01/ex3.asm
+++ prova01/ex3.asm
@@ -9,38 +9,48 @@
 
 main:
     la      $t0, vector         ## $t0 = &vector[]
-    lw 	    $t1, n              ## $t2 = n = 15
-    lw      $t2, ($t0)          ## $t2 = vector[i]
+    lw 	    $t1, n              ## $t2 = n = 5
+    lw      $t2, ($t0)          ## $t2 = vector[i] - Menor Valor do Vetor
     lw      $t3, 4($t0)         ## $t3 = vector[i+1]
     li      $t4, 0              ## $t4 = 1
+	move 	$t6, $t2
+    
+##loop:
+    ##bge     $t4, $t1, exit      ## Se $t0 >= $t2, pula para o exit
+	##lw      $t2, ($t0)          ## $t2 = vector[i]
+
+inner_loop:
+	bge     $t4, $t1, swap      ## Se $t0 >= $t1, pula para o exit
+	lw      $t3, 4($t0)         ## $t3 = vector[i+1]
+	bgt     $t2, $t3, define_index     ## Se $t2 > $t3, pula para o define_index
+	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
+	add 	$t4, $t4, 1 	  ## $t4 = $t4 + 1
+	j       inner_loop
+
+define_index:
+	move 	$t2, $t3 	  ## $t2 = $t3
+	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
+	add 	$t4, $t4, 1 	  ## $t4 = $t4 + 1
+	j 		inner_loop
 
 swap:
-    sw      $t3, ($t0)          ## vector[i] = vector[i+1]
-    sw      $t2, 4($t0)         ## vector[i+1] = vector[i]
-    lw 	    $t2, ($t0)          ## $t2 = vector[i]
-    lw      $t3, 4($t0)         ## $t3 = vector[i+1]
-    add     $t0, $t0, 4         ## $t0 = $t0 + 4
-    add     $t4, $t4, 1         ## $t4 = $t4 + 1
-    j       loop
-    
-loop:
-    bge     $t4, $t1, exit      ## Se $t0 >= $t2, pula para o exit
-    bgt     $t2, $t3, swap      ## if (vector[i] > vector[i+1])
-    add     $t0, $t0, 4         ## $t0 = $t0 + 4
-    add     $t4, $t4, 1         ## $t4 = $t4 + 1
-    j       loop
+	add 	$t0, $t0, -20  	  ## $t0 = $t0 - 4
+	sw      $t2, ($t0)          ## vector[i] = $t2
+	sw 		$t6, 12($t0)         ## vector[i+1] = $t3
+
+	li		$v0, 1
+	move	$a0, $t2
+	syscall
+
+	j       exit
+
+
+##exit_loop:
+	##add     $t0, $t0, 4         ## $t0 = $t0 + 4
+    ##add     $t4, $t4, 1         ## $t4 = $t4 + 1
+    ##j       loop
 
 exit:
 
-    li      $v0, 1              ## Imprime inteiro
-    move    $a0, $t2            ## $a0 = $t2
-    syscall
-
-    li      $v0, 1              ## Imprime inteiro
-    move    $a0, $t3            ## $a0 = $t5
-    syscall
-
     li      $v0, 10             ## $v0 = 10
     syscall
-
-
