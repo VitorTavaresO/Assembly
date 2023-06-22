6124b92dbe67fec0b9dfeb7e86a3e86153e6921a
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-22 18:44:53 -0300

    Worked

--- prova01/ex3.asm
+++ prova01/ex3.asm
@@ -1,57 +1,77 @@
 .data
-    vector: .word  18 51 61 2 33
-    n:  .word 5
-    vector_string: .asciiz "Vetor: "
-    vector_ordened_String: .asciiz "Vetor ordenado: "
-    comma: .asciiz ","
+    vector: .word 7 6 3 2 1 9 8 5 4 0
+    n: .word 10
     newline: .asciiz "\n"
+	string_vector: .asciiz "Vetor Original:\n"
+    string_vector2: .asciiz "Vetor Ordenado:\n"
+
 .text
-
 main:
-    la      $t0, vector         ## $t0 = &vector[]
-    lw 	    $t1, n              ## $t2 = n = 5
-	add 	$t7, $t1, -1 	  ## $t7 = $t1 - 1 = 4
-	li 		$t9, 1
+    la $t0, vector
+    lw $t2, ($t0)
+    li $t3, 0
+    lw $t5, n
+	li $t8, 0
+	la $s0, vector
 
-outer_loop:	
-	bgt     $t9, $t1, exit      ## Se $t9 >= $t1, pula para o exit
-	lw      $t2, ($t0)          ## $t2 = vector[i] - Menor Valor do Vetor
-	move 	$t6, $t2
-	li 		$t4, 0
-	
+outer_loop:
+    bge $t3, $t5, end_outer_loop
+    la $t1, ($t0)
+    add $t4, $t3, $zero
+    lw $t2, ($t0)
+
 inner_loop:
-	bgt     $t4, $t1, swap    ## Se $t4 > $t7, pula para o swap
-	lw      $t3, 4($t0)         ## $t3 = vector[i+1]
-	bgt     $t2, $t3, define_index     ## Se $t2 > $t3, pula para o define_index
-	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
-	add 	$t4, $t4, 1 	  ## $t4 = $t4 + 1
-	j       inner_loop
+    bge $t4, $t5, swap
+    lw $t6, ($t1)
+    add $t4, $t4, 1
+    blt $t6, $t2, smaller
 
-define_index:
-	move 	$t2, $t3 	  ## $t2 = $t3
-	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
-	add 	$t4, $t4, 1 	  ## $t4 = $t4 + 1
-	mul     $t5, $t4, 4         ## $t5 = $t4 * 4
-	j 		inner_loop
+    add $t1, $t1, 4
+    j inner_loop 
 
+smaller:
+    add $t2, $t6, $zero
+    la $t7, ($t1)
+    add $t1, $t1, 4
+    j inner_loop
+    
 swap:
-	mul 	$t8, $t4, 4 	  ## $t8 = $t1 * 4 = -20
-	sub 	$t0, $t0, $t8  	  ## $t0 = $t0 - $t8
-	sw      $t2, ($t0)       ## vector[i] = $t2
-	add 	$t0, $t0, $t5 	## $t0 = $t0 + $t5
-	sw 		$t6, ($t0)        ## vector[i+1] = $t3
-	sub 	$t0, $t0, $t5 	## $t0 = $t0 - $t5
+    la $s0, ($t0)
+    lw $s1, ($s0)
 
+    beq $s1, $t2, equal
+
+    sw $t2, ($t0)
+    sw $s1, ($t7)
+
+equal:
+    add $t0, $t0, 4
+    add $t3, $t3, 1
+    j outer_loop
+    
 end_outer_loop:
-	add 	$t0, $t0, 4 	  ## $t0 = $t0 + 4
-	add 	$t9, $t9, 1 	  ## $t9 = $t9 + 1
+    la $s0, vector
+    li $t1, 1
+    lw $t2, n
+   
+print:
+    bge     $t8, $t5, exit ## Se $t2 >= $t1, pula para o loop_vector
 
-	li 		$v0, 1
-	move 	$a0, $t2
-	syscall
-	j 		outer_loop
-
-exit:
-
-    li      $v0, 10             ## $v0 = 10
+    lw      $t2, ($s0) 	        ## $t2 = vetor[$t1]
+	
+    li      $v0, 1              ## Imprime inteiro
+    move    $a0, $t2            ## $a0 = $t3
     syscall
+
+    li      $v0, 4              ## Imprime string
+    la      $a0, newline          ## $a0 = &comma
+    syscall
+
+    add     $t8, $t8, 1         ## Incrementa o contador do laço
+    add     $s0, $s0, 4         ## $t1 = $t1 + 4 (Move para o próximo elemento do vetor)
+    j       print       ## Volta para o início do laço
+
+   
+exit:
+    li $v0, 10  # syscall para sair do programa
+	syscall
