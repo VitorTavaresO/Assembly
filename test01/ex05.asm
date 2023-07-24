e53c2596e24008476b32b23c4f75bb2bf20456fc
VitorTavares <vitortavares.o55@gmail.com>
2023-07-24 12:52:25 -0300

    Ex04 e Ex05

--- test01/ex05.asm
+++ test01/ex05.asm
@@ -2,7 +2,7 @@
 
     vector: .word 27 58 93 14 76 41 85 3 67 9
     vectorSize: .word 10
-    vectorSum: .word 0
+    vectorAverage: .word 0
 
 .text
 
@@ -13,14 +13,17 @@
 main:
     lw      $t1, ($t0)
     bgt		$t2, $t3, sum
-    sw      $t4, vectorSum
-    j		exit
+    j		average
 
 sum:
     add     $t4, $t1, $t4
     add     $t0, $t0, 4
     add     $t3, $t3, 1
     j       main
+
+average:
+    div     $t4, $t4, $t2
+    sw      $t4, vectorAverage
    
 exit:
     li      $v0, 10
