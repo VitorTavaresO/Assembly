5da8184e0103fd0e5a8b2d907c314f2421a7430b
VitorTavares <vitortavares.o55@gmail.com>
2023-07-24 12:47:30 -0300

    ex04

---
+++ test01/ex04.asm
@@ -0,0 +1,27 @@
+.data
+
+    vector: .word 27 58 93 14 76 41 85 3 67 9
+    vectorSize: .word 10
+    vectorSum: .word 0
+
+.text
+
+    la $t0, vector
+    lw $t2, vectorSize
+    li $t3, 0
+    
+main:
+    lw $t1, ($t0)
+    bgt		$t2, $t3, sum
+    sw      $t4, vectorSum
+    j		exit
+
+sum:
+    add     $t4, $t1, $t4
+    add     $t0, $t0, 4
+    add     $t3, $t3, 1
+    j       main
+   
+exit:
+    li      $v0, 10
+    syscall
