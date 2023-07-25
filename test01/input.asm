6cf4f1736c39a9e3755e881c3d01ac46eaea67ce
VitorTavaresO <vitortavares.o55@gmail.com>
2023-07-25 14:19:41 -0300

    Input Extra

--- test01/input.asm
+++ test01/input.asm
@@ -1,9 +1,20 @@
 .data
+    input_str: .asciiz "Enter a number: "
+    input_str2: .asciiz "Enter another number: "
+    output_str: .asciiz "The sum is: "
 
 .text
+    li $v0, 4
+    la $a0, input_str
+    syscall
+
     li $v0, 5
     syscall
     move $t0, $v0
+
+    li $v0, 4
+    la $a0, input_str2
+    syscall
 
     li $v0, 5
     syscall
@@ -11,7 +22,11 @@
 
     add $t2, $t0, $t1
 
+    li $v0, 4
+    la $a0, output_str
+    syscall
+
     li $v0, 1
     move $a0, $t2
     syscall
-    
+
